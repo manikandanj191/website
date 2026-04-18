06981fa60e1fdba9ee17682f60ff74510f4886b1
manikandanj191 <manikandanj191@gmail.com>
2026-04-18 18:45:20 +0530

    Create About.jsx

---
+++ About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h3 style={{color:'white', backgroundColor:'black',height:'60px',}}>Welcome to Quick Consultants.</h3>
+<p>At Quick Consultants we pride ourselves on delivering a tailored and personalised service to all clients and businesses. We have significant experience in working with businesses of all sizes supporting businesses and their owners all over the country supporting with all areas of accounting, tax, financial services, and day to day business support.
+
+Quick Consultants work with an individual pricing structure, ensuring fees are outlined, broken down and are clear at the beginning of the year to ensure that there are no surprise invoice nor fees at the end of the year.
+
+We have a range of clients and work with businesses of all industries, shapes and sizes. So, whether you are a start-up, sole trader or have been established for years we have an experienced team that will provide you with a fantastic end to end service supporting you as your business grows providing expert knowledge on strategic planning, financial forecasting and business growth.
+
+We offer a variety of end to end accountancy services. No matter whether you are a sole-trader, partnership or a limited company we are here on hand.
+We offer personalised services to help you and your business grow.
+  </p>  
+
+  <img className='second' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA459xaSZDre4v07sy4Tb5_UO_PQqgAMwuiQ&s" alt="img" />
+ 
+ 
+ 
+  </div>
+  )
+}
